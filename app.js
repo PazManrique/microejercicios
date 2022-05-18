cc020f779e8f6b3f7f7d131b04bcb0d594b9ba68
Paz Manrique <nautilus1985@gmail.com>
2022-05-19 00:45:22 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,22 @@
+const myHeading = document.querySelector('h1');
+myHeading.textContent = 'Hello world!';
+
+
+
+
+const myadd = document.querySelector('h3');
+myadd.textContent = ('El resultado de sumar 3+5 es ' + (3+5));
+
+
+
+
+let nombreDeUsuario = prompt ('Introduce tu nombre de usuario');
+
+
+
+
+let firstNumber = prompt ('Introduce el primer numero');
+let secondNumber = prompt ('Introduce el segundo numero');
+const result = document.querySelector('p');
+result.textContent = 'El resultado de sumar los numeros introducidos es ' + (parseInt(firstNumber) + parseInt(secondNumber));
+ 
